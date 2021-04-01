6fee8b134ab69a8e19a3fa41e1be5175c652bf40
andoryu <andrewhowa@gmail.com>
2021-04-01 20:03:09 +1000

    Basic test map object loading from json

--- src/AntMap.py
+++ src/AntMap.py
@@ -1,3 +1,4 @@
+import json
 
 
 NORTH = 1
@@ -5,16 +6,227 @@ EAST  = 2
 SOUTH = 3
 WEST  = 4
 
+BOUNDED = 1
+UNBOUNDED = 2
+
 class AntMap:
-    def __init__(self, map_definition=None):
+    def __init__(self, map_definition_file=""):
         self.ant_location = (0,0, EAST)
-        self.start_location = (0,0)
-        self.end_location   = (0,0)
-        self.map = ()
+        self.ant_facing = EAST
+        self.end_location   = [0,0]
+        self.map = []
+        self.size = (0,0)
+        self.style = BOUNDED
+        self.name = "unnamed"
+        self.start_location = [0,0]
 
-        if map_definition:
-            self._load_definition(map_definition)
+        self.loaded = False
+
+        if map_definition_file:
+            self.loaded = self.load_definition(map_definition_file)
+
+    def ant_advance(self):
+        facing  = self.ant_location[2]
+
+        if facing == NORTH:
+            shift = (  0, -1 )
+        if facing == EAST:
+            shift = (  1,  0 )
+        if facing == SOUTH:
+            shift = (  0,  1 )
+        if facing == WEST:
+            shift = ( -1,  0 )
+
+        new_x = self.ant_location[0] + shift[0]
+        new_y = self.ant_location[1] + shift[1]
+
+        #bounds check
+        if new_x < 0:
+            new_x = 0
+        elif new_x >= self.size[0]:
+            new_x = self.size[0]
+
+        if new_y < 0:
+            new_y = 0
+        elif new_y >= self.size[1]:
+            new_y = self.size[1]
+
+        self.ant_location = (new_x, new_y, facing)
+
+        #return location and view info
+        return {
+            "loc":  self.ant_location,
+            "view": self.get_ant_view()
+        }
 
 
-    def _load_definition(map_definition):
-        pass
+    def ant_rotate_ccw(self):
+        facing  = self.ant_location[2]
+
+        if facing == NORTH:
+            new_facing = WEST
+        if facing == EAST:
+            new_facing = NORTH
+        if facing == SOUTH:
+            new_facing = EAST
+        if facing == WEST:
+            new_facing = SOUTH
+
+        self.ant_location = (self.ant_location[0], self.ant_location[1], new_facing)
+
+        #return location and view info
+        return {
+            "loc":  self.ant_location,
+            "view": self.get_ant_view()
+        }
+
+
+    def ant_rotate_cw(self):
+        facing  = self.ant_location[2]
+
+        if facing == NORTH:
+            new_facing = EAST
+        if facing == EAST:
+            new_facing = SOUTH
+        if facing == SOUTH:
+            new_facing = WEST
+        if facing == WEST:
+            new_facing = NORTH
+
+        self.ant_location = (self.ant_location[0], self.ant_location[1], new_facing)
+
+        #return location and view info
+        return {
+            "loc":  self.ant_location,
+            "view": self.get_ant_view()
+        }
+
+    def _convert_ant_facing(self, facing):
+        if facing.upper() == "NORTH":
+            return NORTH
+        elif facing.upper() == "EAST":
+            return EAST
+        elif facing.upper() == "SOUTH":
+            return SOUTH
+        elif facing.upper() == "WEST":
+            return WEST
+        else:
+            raise ValueError("Value ''" +facing+ "' for style is not understood")
+
+
+    def _convert_map_style(self, style_string):
+        if style_string.upper() == "BOUNDED":
+            return BOUNDED
+        elif style_string.upper() == "UNBOUNDED":
+            return UNBOUNDED
+        else:
+            raise ValueError("Value ''" +style_string+ "' for style is not understood")
+
+
+    def get_ant_view(self):
+        facing  = self.ant_location[2]
+        position= (self.ant_location[0], self.ant_location[1])
+
+        #sift is a triplet of offsets of left, middle, right view for ant facing
+        if facing == NORTH:
+            shift = ( (-1,-1), (0,-1), (1,-1) )
+        if facing == EAST:
+            shift = ( (1,-1), (1,0), (1,1) )
+        if facing == SOUTH:
+            shift = ( (1,1), (0,1), (-1,1) )
+        if facing == WEST:
+            shift = ( (-1,1), (-1,0), (-1,-1) )
+
+        #return dict
+        return {
+            "left":     self._get_offset_pheremone( position, shift[0] ),
+            "middle":   self._get_offset_pheremone( position, shift[1] ),
+            "right":    self._get_offset_pheremone( position, shift[2] ),
+        }
+
+
+    def _get_offset_pheremone(self, position, offset):
+        x = position[0]+offset[0]
+        y = position[1]+offset[1]
+
+        #check bounds
+        if x < 0 or x >= self.size[0]:
+            return None
+
+        if y < 0 or y >= self.size[1]:
+            return None
+
+        return self.map[x][y]
+
+
+    def load_definition(self, map_definition_file):
+        try:
+            f = open(map_definition_file)
+            cfg = json.load(f)
+
+            #configure the map based on the json file
+            self.name = cfg["name"]
+
+            self.style = self._convert_map_style(cfg["style"])
+            self.size = tuple(cfg["size"])
+
+            #setup the map size
+            #self.map = ( () for _ in range(self.size[1]) )
+            col = list( False for _ in range(self.size[1]) )
+            self.map = list(col for _ in range(self.size[0]))
+
+            #load the trail
+            trail = cfg["trail"]
+            for pheremone in trail:
+                self.map[pheremone[0]][pheremone[1]] = True
+
+            #start location
+            self.start_location = cfg["start_location"]
+
+            #start location
+            self.end_location = cfg["end_location"]
+
+            self.ant_facing = self._convert_ant_facing(cfg["ant_facing"])
+
+            f.close()
+            self.loaded = True
+            return True
+
+        except FileNotFoundError:
+            print("File " + map_definition_file + "not found.")
+            return False
+
+        except json.decoder.JSONDecodeError as e:
+            print("File " + map_definition_file + "is not a valid JSON file.")
+            print(e)
+            return False
+
+        except KeyError as e:
+            print(e)
+            return False
+
+        except ValueError as e:
+            print(e)
+            return False
+
+
+    def load_state(self):
+        return self.loaded
+
+    def raw_print(self):
+        for y in range(self.size[1]):
+            for x in range(self.size[0]):
+                if self.map[x][y]:
+                    print("*", end="")
+                else:
+                    print("0", end="")
+            print("")
+
+    def reset(self):
+        self.ant_location = (self.start_location[0], self.start_location[1],
+            self.ant_facing)
+
+
+if __name__ == "__main__":
+    map = AntMap("test/basic_map.json")
+    map.raw_print()
--- test/test_antmap.py
+++ test/test_antmap.py
@@ -13,11 +13,161 @@ def ant_map():
     return AntMap.AntMap()
 
 @pytest.fixture
-def map_json():
-    return json.load(file("basic_map.json"))
+def ant_map_w_good_file():
+    return AntMap.AntMap("test/basic_map.json")
 
 def test_defaults(ant_map):
-    assert ant_map.start_location == (0,0)
-    assert ant_map.end_location == (0,0)
-    assert ant_map.ant_location == (0,0, AntMap.EAST)
-    assert ant_map.map == ()
+    assert ant_map.start_location == [0,0]
+    assert ant_map.end_location == [0,0]
+    assert ant_map.ant_location == (0, 0, AntMap.EAST)
+    assert ant_map.map == []
+    assert ant_map.style == AntMap.BOUNDED
+    assert ant_map.loaded == False
+
+
+def test_missing_def_file(ant_map):
+    assert ant_map.load_definition("absent_test_file123.json") == False
+    assert ant_map
+
+
+def test_invalid_json_file(ant_map, tmp_path):
+    json_file = tmp_path / "temp.json"
+    json_file.write_text('{ "a": 1 "b": 2 }')
+
+    assert ant_map.load_definition(json_file.as_posix()) == False
+    assert ant_map
+
+def test_incorrect_antmap_file(ant_map, tmp_path):
+    json_file = tmp_path / "temp.json"
+    json_file.write_text('{ "a": 1 }')
+
+    assert ant_map.load_definition(json_file.as_posix()) == False
+    assert ant_map
+
+
+def test_style_conversion(ant_map):
+    assert ant_map._convert_map_style("BOUNDED") == AntMap.BOUNDED
+    assert ant_map._convert_map_style("UNBOUNDED") == AntMap.UNBOUNDED
+    assert ant_map._convert_map_style("bounded") == AntMap.BOUNDED
+    assert ant_map._convert_map_style("unbounded") == AntMap.UNBOUNDED
+    with pytest.raises(ValueError):
+        assert ant_map._convert_map_style("INFINITY")
+
+def test_facing_conversion(ant_map):
+    assert ant_map._convert_ant_facing("NORTH") == AntMap.NORTH
+    assert ant_map._convert_ant_facing("EAST") == AntMap.EAST
+    assert ant_map._convert_ant_facing("SOUTH") == AntMap.SOUTH
+    assert ant_map._convert_ant_facing("WEST") == AntMap.WEST
+
+    assert ant_map._convert_ant_facing("north") == AntMap.NORTH
+    assert ant_map._convert_ant_facing("east") == AntMap.EAST
+    assert ant_map._convert_ant_facing("south") == AntMap.SOUTH
+    assert ant_map._convert_ant_facing("west") == AntMap.WEST
+
+    with pytest.raises(ValueError):
+        assert ant_map._convert_ant_facing("UP")
+
+def test_map_load(ant_map_w_good_file):
+    assert ant_map_w_good_file
+
+    assert ant_map_w_good_file.name == "Basic Test Trail"
+    assert ant_map_w_good_file.style == AntMap.BOUNDED
+    assert ant_map_w_good_file.size  == (4,4)
+
+    col = list(False for _ in range(4))
+    map = list(col for _ in range(4))
+    for x in range(4):
+        map[x][2] = True
+    assert ant_map_w_good_file.map  == map
+
+    assert ant_map_w_good_file.start_location == [0,2]
+    assert ant_map_w_good_file.end_location   == [3,2]
+
+    assert ant_map_w_good_file.ant_facing == AntMap.EAST
+
+def test_reset(ant_map_w_good_file):
+    ant_map_w_good_file.reset()
+    assert ant_map_w_good_file.ant_location == (0,2,AntMap.EAST)
+
+
+def test__get_offset_pheremone(ant_map_w_good_file):
+    ant_map = ant_map_w_good_file
+
+    #facing NORTH ( (-1,-1), (0,-1), (1,-1) )
+    assert ant_map._get_offset_pheremone( (0,2), (-1,-1)) == None
+    assert ant_map._get_offset_pheremone( (0,2), (0, -1)) == False
+    assert ant_map._get_offset_pheremone( (0,2), (1, -1)) == False
+
+    #facing EAST ( (1,-1), (1,0), (1,1) )
+    assert ant_map._get_offset_pheremone( (0,2), (1,-1)) == False
+    assert ant_map._get_offset_pheremone( (0,2), (1, 0)) == True
+    assert ant_map._get_offset_pheremone( (0,2), (1, 1)) == False
+
+    #facing SOUTH ( (1,1), (0,1), (-1,1) )
+    assert ant_map._get_offset_pheremone( (0,2), (1,1)) == False
+    assert ant_map._get_offset_pheremone( (0,2), (0,1)) == False
+    assert ant_map._get_offset_pheremone( (0,2), (-1,1)) == None
+
+    #facing WEST ( (-1,1), (-1,0), (-1,-1) )
+    assert ant_map._get_offset_pheremone( (0,2), (-1, 1)) == None
+    assert ant_map._get_offset_pheremone( (0,2), (-1, 0)) == None
+    assert ant_map._get_offset_pheremone( (0,2), (-1,-1)) == None
+
+def test_get_ant_view_and_actions(ant_map_w_good_file):
+    ant_map = ant_map_w_good_file
+    ant_map.reset()
+
+    assert ant_map.get_ant_view() == {
+        "left":     False,
+        "middle":   True,
+        "right":    False,
+    }
+
+    assert ant_map.ant_advance() == {
+        "loc": (1,2,AntMap.EAST),
+        "view": {
+            "left":     False,
+            "middle":   True,
+            "right":    False
+        }
+    }
+
+    assert ant_map.ant_rotate_ccw() == {
+        "loc": (1,2,AntMap.NORTH),
+        "view": {
+            "left":     False,
+            "middle":   False,
+            "right":    False
+        }
+    }
+
+    assert ant_map.ant_advance() == {
+        "loc": (1,1,AntMap.NORTH),
+        "view": {
+            "left":     False,
+            "middle":   False,
+            "right":    False
+        }
+    }
+
+    assert ant_map.ant_advance() == {
+        "loc": (1,0,AntMap.NORTH),
+        "view": {
+            "left":     None,
+            "middle":   None,
+            "right":    None
+        }
+    }
+
+    assert ant_map.ant_rotate_cw() == {
+        "loc": (1,0,AntMap.EAST),
+        "view": {
+            "left":     None,
+            "middle":   False,
+            "right":    False
+        }
+    }
+
+
+#def test_map_load(ant_map_w_bad_file):
+#    assert ant_map_w_file
